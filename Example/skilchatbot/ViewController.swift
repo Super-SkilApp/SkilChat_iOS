4e2858a21002657901f5babcea149985543a569b
Designed screens and font size-family-style <>
2019-06-18 18:06:12 +0530

    Initial commit

---
+++ Example/skilchatbot/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  skilchatbot
+//
+//  Created by Designed screens and font size-family-style. on 06/18/2019.
+//  Copyright (c) 2019 Designed screens and font size-family-style.. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
