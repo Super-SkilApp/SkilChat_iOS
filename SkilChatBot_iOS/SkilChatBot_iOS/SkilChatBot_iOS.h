1af5f9c7eb9703bdf46cb29f60cc9b92c51ac65b
Designed screens and font size-family-style <>
2019-06-25 11:40:07 +0530

    chat app

---
+++ SkilChatBot_iOS/SkilChatBot_iOS/SkilChatBot_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SkilChatBot_iOS.h
+//  SkilChatBot_iOS
+//
+//  Created by Uday  on 24/06/19.
+//  Copyright © 2019 Uday . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SkilChatBot_iOS.
+FOUNDATION_EXPORT double SkilChatBot_iOSVersionNumber;
+
+//! Project version string for SkilChatBot_iOS.
+FOUNDATION_EXPORT const unsigned char SkilChatBot_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SkilChatBot_iOS/PublicHeader.h>
+
+
---
+++ SkilChatBot_iOS/SkilChatBot_iOS/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SkilChatBot_iOS
+//
+//  Created by Uday  on 24/06/19.
+//  Copyright © 2019 Uday . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func ChatAction(_ sender: Any)
+    {
+        print("Chat action is Ok")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
